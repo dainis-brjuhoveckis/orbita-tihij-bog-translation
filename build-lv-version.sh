8a4035265e848d4da7dceed83161019f90facb29
Dainis Brjuhoveckis <dainis.brjuhoveckis@gmail.com>
2020-08-23 11:03:16 +0300

    initial version of the lv text file creation script

--- build-lv-version.sh
+++ build-lv-version.sh
@@ -3,3 +3,4 @@
 # take every second line from the source file orbita.tihij-bog.lyrics.ru-lv.txt and output to file orbita.tihij-bog.lyrics.lv.txt
 # idea: use sed
 
+ sed '1~2d' orbita.tihij-bog.lyrics.ru-lv.txt > orbita.tihij-bog.lyrics.lv.txt
